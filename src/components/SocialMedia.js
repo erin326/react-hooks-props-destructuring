8e619d479802b54985e45563c9c6a0ee0eeaf3a0
erin <erinverkoulen93@gmail.com>
2021-07-02 11:38:38 -0500

    commit tree 60171aa35afe07d76e646f93eb20bd66800f3857

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SocialMedia({socialLinks : {github, linkedin}}) {
+    return (
+        <div>
+            <a href={github}>{github}</a>
+            <a href={linkedin}>{linkedin}</a>
+        </div> 
+    )
+}
+export default SocialMedia;
